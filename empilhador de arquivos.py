8e703d1de76de2781fad9a19da6bd20094c420b9
Gustavo-Herreira <57445748+Gustavo-Herreira@users.noreply.github.com>
2019-11-28 14:36:09 -0300

    Update empilhador de arquivos.py
    Adicionado comentários e espaçamento entre os procedimentos
    
    Apenas adaptei um código já existente, disponível em: https://www.studytonight.com/post/converting-xlsx-file-to-csv-file-using-python

--- empilhador de arquivos.py
+++ empilhador de arquivos.py
@@ -1,38 +1,50 @@
 import os
-from typing import List, Any
-
 import openpyxl
 
 files = []
+
 empilhado = open('empilhado.txt', 'a+', errors='ignore')
 for path, diretorio, arquivos in os.walk('C:/Users/gustavo.gomes/Desktop/empilhador'):
     for file in arquivos:
         if ('.txt' in file or '.csv' in file) and 'empilhado' not in file:
             files.append(file)
+
         elif '.xlsx' in file:
             file = file.replace('.xlsx', '')
+
             filename = f'{file}.xlsx'
+
             # Abre o arquivo xlsx
             xlsx = openpyxl.load_workbook(filename)
+
             # Ativa a planilha atual
             sheet = xlsx.active
+
             # Pega o conteudo da planilha
             data = sheet.rows
+
             # Cria o arquivo CSV
             csv = open(f"{file}.csv", "w+", errors='ignore')
+
             for row in data:
-                l: List[linhas] = list(row)
+                l = list(row)
                 for i in range(len(l)):
                     if i == len(l) - 1:
                         csv.write(str(l[i].value))
                     else:
                         csv.write(str(l[i].value) + ';')
                 csv.write('\n')
+
             csv.close()
             files.append(f'{file}.csv')
+
+
+
+
 for file in files:
     with open('C:/Users/gustavo.gomes/Desktop/empilhador/' + file, 'r', encoding='latin-1', errors='ignore') as arquivo:
         lines = arquivo.readlines()
         for line in lines:
             empilhado.write(line)
+
 print('arquivos lidos:', files)
